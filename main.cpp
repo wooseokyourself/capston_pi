69c1fc6940c0c2656f4bf50b837b72ef5a02b5e1
wooseokyourself <21500740@handong.edu>
2020-02-03 07:12:38 +0900

    image transfer

--- main.cpp
+++ main.cpp
@@ -54,35 +54,11 @@ int main(int argc, char *argv[])
 	}
 
 	while (1) {
-		printf("\033[1;33m명령어 : get, put, pwd, ls, cd, quit\n");
+		printf("\033[1;33m명령어 : put, quit\n");
 		printf("\033[1;32mclient> ");
 		fgets(bufmsg, MAXLINE, stdin); //명령어 입력
 		fprintf(stderr, "\033[97m");   //글자색을 흰색으로 변경
-		if (!strcmp(bufmsg, "get\n")) {//get명령어를 입력받았다면
-			printf("다운로드할 파일 : ");
-			scanf("%s", filename);       //파일 이름 입력
-			fgets(temp, MAXLINE, stdin); //버퍼에 남은 엔터 제거
-			strcpy(buf, "get ");
-			strcat(buf, filename);
-			send(sock, buf, 100, 0);         //명령어 전송
-			recv(sock, &size, sizeof(int), 0);
-			if (!size) {//파일이 없다면
-				printf("파일이 없습니다\n");
-				continue;
-			}
-			f = (char *)malloc(size);
-			recv(sock, f, size, 0);
-			while (1) {
-				filehandle = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0666);
-				if (filehandle == -1) //같은 이름이 있다면 이름 끝에 _1 추가
-					sprintf(filename + strlen(filename), "_1");
-				else break;
-			}
-			write(filehandle, f, size);
-			close(filehandle);
-			printf("다운로드 완료\n");//전송이 잘 되었다면
-		}
-		else if (!strcmp(bufmsg, "put\n")) {//put명령어를 입력받았다면
+		if (!strcmp(bufmsg, "put\n")) {//put명령어를 입력받았다면
 			printf("업로드할 파일 : ");
 			scanf("%s", filename);       //파일 이름 입력
 			fgets(temp, MAXLINE, stdin); //버퍼에 남은 엔터 제거
@@ -93,46 +69,26 @@ int main(int argc, char *argv[])
 			}
 			strcpy(buf, "put ");
 			strcat(buf, filename);
+			
+			/* 이 send() 는 서버코드에서 무한루프의 시작인 recv() 가 받는다. */
 			send(sock, buf, 100, 0);
+
 			stat(filename, &obj);
 			size = obj.st_size;
+
+			/**/
 			send(sock, &size, sizeof(int), 0);//명령어 전송
+
+			/**/
 			sendfile(sock, filehandle, NULL, size);//파일 전송
-			recv(sock, &status, sizeof(int), 0);
+
+			/**/
+			recv(sock, &status, sizeof(int), 0); //서버로부터 파일이 잘 쓰였는지를 받음
+
 			if (status)//업로드가 잘 되었다면
 				printf("업로드 완료\n");
 			else
 				printf("업로드 실패\n");
-		}
-		else if (!strcmp(bufmsg, "pwd\n")) {//pwd명령어를 입력받았다면
-			strcpy(buf, "pwd");
-			send(sock, buf, 100, 0);
-			recv(sock, buf, 100, 0);
-			printf("--The path of the Remote Directory--\n%s", buf);
-		}
-		else if (!strcmp(bufmsg, "ls\n")) {//ls명령어를 입력받았다면
-			strcpy(buf, "ls");
-			send(sock, buf, 100, 0);
-			recv(sock, &size, sizeof(int), 0);
-			f = (char *)malloc(size);
-			recv(sock, f, size, 0);
-			filehandle = creat("temp.txt", O_WRONLY);
-			write(filehandle, f, size);
-			close(filehandle);
-			printf("--The Remote Directory List--\n");
-			system("cat temp.txt");	//현재 디렉토리의 파일 출력
-		}
-		else if (!strcmp(bufmsg, "cd\n")) {//cd명령어를 입력받았다면
-			strcpy(buf, "cd ");
-			printf("이동할 경로 이름 : ");
-			scanf("%s", buf + 3);        //위치 입력
-			fgets(temp, MAXLINE, stdin); //버퍼에 남은 엔터 제거
-			send(sock, buf, 100, 0);     //명령어 전송
-			recv(sock, &status, sizeof(int), 0);
-			if (status)
-				printf("경로 변경 완료\n");
-			else
-				printf("경로 변경 실패\n");
 		}
 		else if (!strcmp(bufmsg, "quit\n")) {//quit명령어를 입력받았다면
 			strcpy(buf, "quit");
