0f91f83c6b70193d7c334c028da625f7337d8500
CHOIWOOSEOK <21500740@handong.edu>
2020-02-09 17:34:10 +0900

    update

--- main.cpp
+++ main.cpp
@@ -13,13 +13,3 @@ main (void) {
     }
     return 0;
 }
-
-
-/*  파이카메라로 이벤트를 감지하고, 그 이벤트를 촬영하여 .jpeg 파일로 저장한뒤 저장된 파일명을 리턴 */
-string ImageProcessing () {
-    string fileName;
-    
-    //  작성
-    
-    return fileName;
-}
